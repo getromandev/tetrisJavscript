9d718b80d3b1e17097ef49367893b0d7bc929c09
getromandev <getroman.dev@gmail.com>
2019-12-28 12:37:14 -0500

    wrap darawMatrix in a function with offset functionality

--- tetris.js
+++ tetris.js
@@ -18,19 +18,24 @@ const matrix = [
     [0, 1, 0]
 ];
 
+// wrap the matrix draw in a function with matrix and offset as parameters
+function drawMatrix(matrix, offset) {
 // draw this first piece to the canvas
-matrix.forEach((row, y) => {
-    console.log('this is the row', row)
-    console.log('this is the y', y);
+    matrix.forEach((row, y) => {
+        console.log('this is the row', row)
+        console.log('this is the y', y);
 
-    row.forEach((value, x) => {
-        console.log('this is the value', value)
-        console.log('this is the x', x);
+        row.forEach((value, x) => {
+            console.log('this is the value', value)
+            console.log('this is the x', x);
 
-        // if the value is not 0 then we draw
-        if (value !== 0) {
-            context.fillStyle = 'red';
-            context.fillRect(x, y, 1, 1);
-        }
+            // if the value is not 0 then we draw
+            if (value !== 0) {
+                context.fillStyle = 'red';
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        })
     })
-})
+}
+
+drawMatrix(matrix, {x: 5, y: 5});
