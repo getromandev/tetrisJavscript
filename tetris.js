27bb6b41f2334ce33e3fdce5ad617f0cec9bd4f8
getromandev <getroman.dev@gmail.com>
2019-12-29 22:26:59 -0500

    create a function that selects random tertris piece from createPiece function

--- tetris.js
+++ tetris.js
@@ -130,7 +130,7 @@ function playerDrop() {
     if (collide(arena, player)) {
         player.pos.y --;
         merge(arena, player);
-        player.pos.y = 0;
+        playerReset();
     }
     dropCounter = 0;
 }
@@ -141,6 +141,15 @@ function playerMove(dir) {
     if (collide(arena, player)) {
         player.pos.x -= dir;
     }
+}
+
+// create a function that selects random pieces from the createPiece function matrix's 
+function playerReset() {
+    const pieces = 'ILJOTSZ'
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length / 2 | 0) - 
+                   (player.matrix[0].length / 2 | 0);
 }
 
 // function that handles the player rotating the piece 
