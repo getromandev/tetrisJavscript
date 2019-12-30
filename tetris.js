586661ed2413917a1a7efa215d329c51cc830e02
getromandev <getroman.dev@gmail.com>
2019-12-29 21:44:55 -0500

    write a function that rotates the array

--- tetris.js
+++ tetris.js
@@ -103,6 +103,27 @@ function playerMove(dir) {
     }
 }
 
+// write a function that rotates the array 
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x]
+            ] = [
+                matrix[y][x]
+                matrix[x][y]
+            ];
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
 // set up a drop counter to keep track off the time it takes to drop the piece 
 let dropCounter = 0;
 let dropInterval = 1000;
